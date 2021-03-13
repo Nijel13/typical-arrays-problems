72878486922d0853372fead4f23998fbb11fea7c
Nijel13 <nikodim97@gmail.com>
2021-03-13 19:57:46 +0300

    final changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  }
+  let minimum = 0
+  for (let i = 0; i < array.length; i += 1){
+      if (minimum > array[i]){
+        minimum = array[i]
+      }
+  }
+  return minimum
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+  }
+  let maximum = 1
+  for (let j = 0; j < array.length; j += 1){
+      if (array[j] > maximum ){
+        maximum = array[j]
+      } 
+    } 
+    return maximum
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(array === undefined || array.length === 0) {
+    return 0
+  }
+  let average, sum = 0
+    for (let k = 0; k < array.length; k += 1){
+      sum = sum + array[k]
+    }
+    average = sum / array.length
+      return average
+  }
